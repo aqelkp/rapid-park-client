59246ee35f3332c4b2b55de45e4a280099227253
Aqel Ahammad <aqel123@gmail.com>
2016-01-25 21:03:34 +0530

    New user created in firebase

---
+++ app/src/main/java/xyz/brozzz/rapidpark/Fragments/BalanceFragment.java
@@ -0,0 +1,31 @@
+package xyz.brozzz.rapidpark.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import xyz.brozzz.rapidpark.R;
+
+public class BalanceFragment extends Fragment {
+
+    public BalanceFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_balance, container, false);
+        return view;
+    }
+
+}
